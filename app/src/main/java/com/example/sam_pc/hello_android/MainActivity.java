e3e8c92b30e6468208ff131ddef0d7e420e5b373
SamJonson <1328383679@qq.com>
2015-04-03 22:49:07 +0800

    First提交

---
+++ app/src/main/java/com/example/sam_pc/hello_android/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sam_pc.hello_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
